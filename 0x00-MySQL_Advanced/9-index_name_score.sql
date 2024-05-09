23701eeb1b9eeb2173c2d57a13560fac0e641904
Iheb Chaoued <ihebchaoued@gmail.com>
2024-05-09 17:10:37 +0000

    name & score

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,2 +1,2 @@
 -- index idx_name_first_score: name column and score column
-CREATE INDEX idx_name_first_score ON names (name(1)), score);
+CREATE INDEX idx_name_first_score ON names (name(1), score);
