17b7fafbd4817322863cf77cfa2bdef032492161
Iheb Chaoued <ihebchaoued@gmail.com>
2024-05-09 16:57:59 +0000

    Ranking by Longevity

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,5 @@
 -- List bands with Glam rock as main style, ranked by longevity
-SELECT band_name, 
-       (2022 - formed) AS lifespan
+SELECT band_name, (2022 - formed) AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
 ORDER BY lifespan DESC;
