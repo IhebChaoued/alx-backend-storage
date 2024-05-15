ab79e4a37589b9356621c87e514aae2eb4fb32d2
Iheb Chaoued <ihebchaoued@gmail.com>
2024-05-15 15:54:41 +0000

    get

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""get requests"""
+
+import requests
+import redis
+
+
+def get_page(url: str) -> str:
+    """Get the HTML content of a URL."""
+    r = redis.Redis()
+
+    url_key = f"count:{url}"
+    r.incr(url_key)
+
+    cached_content = r.get(url)
+    if cached_content:
+        return cached_content.decode('utf-8')
+
+    response = requests.get(url)
+    html_content = response.text
+
+    r.setex(url, 10, html_content)
+
+    return html_content
