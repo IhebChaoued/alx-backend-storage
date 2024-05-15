c93bf0c7d0578b49848c5c50620a84f6374a3693
Iheb Chaoued <ihebchaoued@gmail.com>
2024-05-15 16:00:22 +0000

    updated

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,11 +12,16 @@ def call_history(method: Callable) -> Callable:
     def wrapper(self, *args, **kwargs):
         input_key = "{}:inputs".format(method.__qualname__)
         output_key = "{}:outputs".format(method.__qualname__)
-
+        
+        # Store input parameters
         self._redis.rpush(input_key, str(args))
+        
+        # Execute the original method to retrieve the output
         output = method(self, *args, **kwargs)
+        
+        # Store the output
         self._redis.rpush(output_key, output)
-
+        
         return output
     return wrapper
 
@@ -31,13 +36,12 @@ class Cache:
 
     @call_history
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """Store the input data in Redis"""
+        """Store the input data in Redis using a random key and return the key"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self, key: str, fn: Callable = None)
-    -> Union[str, bytes, int, float, None]:
+    def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:
         """Retrieve data from Redis"""
         data = self._redis.get(key)
         if data is None:
@@ -53,16 +57,3 @@ class Cache:
     def get_int(self, key: str) -> Union[int, None]:
         """Retrieve data from Redis as an integer"""
         return self.get(key, fn=int)
-
-
-def replay(method: Callable) -> None:
-    """Display history of calls for a specific function."""
-    name = method.__qualname__
-    cache = redis.Redis()
-    calls = cache.get(name).decode("utf-8")
-    print("{} was called {} times:".format(name, calls))
-    inputs = cache.lrange(name + ":inputs", 0, -1)
-    outputs = cache.lrange(name + ":outputs", 0, -1)
-    for i, o in zip(inputs, outputs):
-        print("{}(*{}) -> {}".format(
-            name, i.decode('utf-8'), o.decode('utf-8')))
