671914e5fdf297298dc63e9122b16e0d2682b910
Iheb Chaoued <ihebchaoued@gmail.com>
2024-05-15 16:22:40 +0000

    web

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,38 +1,43 @@
 #!/usr/bin/env python3
-"""web"""
+""" Module for web functions """
+
 import requests
 import redis
+from functools import wraps
 
 
 def track_and_cache(func):
-    """Decorator to track URL access count and cache the result."""
+    """ Decorator to track and cache page requests """
+    @wraps(func)
     def wrapper(url):
-        """Wrapper function to track URL access count and cache result."""
-        r = redis.Redis()
+        """ Wrapper function to track and cache page requests """
+        redis_conn = redis.Redis()
 
-        url_key = f"count:{url}"
-        r.incr(url_key)
+        url_count_key = f"count:{url}"
+        redis_conn.incr(url_count_key)
 
-        cached_content = r.get(url)
+        cached_content = redis_conn.get(url)
         if cached_content:
             return cached_content.decode('utf-8')
 
         response = requests.get(url)
-        html_content = response.text
+        page_content = response.text
 
-        r.setex(url, 10, html_content)
+        redis_conn.setex(url, 10, page_content)
 
-        return html_content
+        return page_content
 
     return wrapper
 
 
 @track_and_cache
 def get_page(url: str) -> str:
-    """Get the HTML content of a URL."""
+    """ Function to get page content """
     return requests.get(url).text
 
 
 if __name__ == "__main__":
-    url = "http://slowwly.robertomurray.co.uk/delay/5000/url/http://www.example.com"
+    url = ("http://slowwly.robertomurray.co.uk/"
+           "delay/1000/url/"
+           "https://www.example.com")
     print(get_page(url))
