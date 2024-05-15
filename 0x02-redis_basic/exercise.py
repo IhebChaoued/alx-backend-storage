85a06b161d47ad564b8b06cce398492584187986
Iheb Chaoued <ihebchaoued@gmail.com>
2024-05-15 16:09:20 +0000

    updated

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -5,12 +5,23 @@ import uuid
 from typing import Union, Callable
 from functools import wraps
 
+
+def count_calls(method: Callable) -> Callable:
+    """Decorator to count the number of times a method is called."""
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+    return wrapper
+
+
 def call_history(method: Callable) -> Callable:
     """Decorator to store the history of inputs and outputs for a function."""
     @wraps(method)
     def wrapper(self, *args, **kwargs):
-        input_key = f"{method.__qualname__}:inputs"
-        output_key = f"{method.__qualname__}:outputs"
+        input_key = "{}:inputs".format(method.__qualname__)
+        output_key = "{}:outputs".format(method.__qualname__)
 
         self._redis.rpush(input_key, str(args))
         output = method(self, *args, **kwargs)
@@ -28,11 +39,18 @@ class Cache:
         self._redis = redis.Redis()
         self._redis.flushdb()
 
-    @call_history
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Store the input data in Redis"""
         key = str(uuid.uuid4())
         self._redis.set(key, data)
+        return key
+
+    @call_history
+    def store_list(self, data: list) -> str:
+        """Store a list of data in Redis"""
+        key = str(uuid.uuid4())
+        self._redis.rpush(key, *data)
         return key
 
     def get(self, key: str, fn: Callable = None) -> Union[str, bytes, int, float, None]:
@@ -51,3 +69,16 @@ class Cache:
     def get_int(self, key: str) -> Union[int, None]:
         """Retrieve data from Redis as an integer"""
         return self.get(key, fn=int)
+
+
+def replay(method: Callable) -> None:
+    """Display history of calls for a specific function."""
+    name = method.__qualname__
+    cache = redis.Redis()
+    calls = cache.get(name).decode("utf-8")
+    print("{} was called {} times:".format(name, calls))
+    inputs = cache.lrange(name + ":inputs", 0, -1)
+    outputs = cache.lrange(name + ":outputs", 0, -1)
+    for i, o in zip(inputs, outputs):
+        print("{}(*{}) -> {}".format(
+            name, i.decode('utf-8'), o.decode('utf-8')))
